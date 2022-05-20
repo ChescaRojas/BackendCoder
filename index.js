5b9efb109dd182d109ec4aac058ca53280dd7372
Fracesca <chescarojas0407@gmail.com>
2022-05-20 09:31:29 -0300

    cuarta entrega

--- index.js
+++ index.js
@@ -1,53 +1,51 @@
-const express = require('express')
+const express = require('express');
+const Container = require('./src/models/Container') 
 const app = express();
-const Usuario = require('./src/models/Usuario'); 
-const Container = require('./src/models/Container'); 
-const libros = [{nombre: 'The notebook', autor: 'Nicholas Sparks'}, {nombre: 'After', autor: 'Anna Todd'}]
-const mascotas = ['gato', 'perro']
+const products = express.Router()
 
-//definimos un puerto para HTTP
-const time = 3000;
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use('/api/productos', products);
 
-app.get('/', (req, res) => {
-    const usuarioUno = new Usuario('Juan', 'Rojas', libros, mascotas)
-    usuarioUno.addBook('Metamorfosis',' Franz Kafka')
-    usuarioUno.addPet('Loro')
-    res.send(usuarioUno.getFullName());
-    //, usuarioUno.countPets(), usuarioUno.getBookNames()
+products.post('/', (req, res)=> {
+  const file = new Container("Archivo");
+  const allCalls = async () => {
+    const callOne = await file.save({title: req.body.name , price: req.body.price , thumbnail: req.body.image})
+    res.send(callOne)
+  }
+  allCalls()
 });
-app.get('/segunda', (req, res) => {
-    const file = new Container("Archivo");
-    const allCalls = async () => {
-      const callOne = await file.save({title: 'titulo', price: 123, thumbnail: 'url de foto'})
-      const callTwo = await file.getById(2)
-      const callThree = await file.getAll()
-      await file.deleteById(4)
-      //await file.deleteAll()
-      res.send(`callOne: ${callOne} 
-        callTwo: {${callTwo?.id}, ${callTwo?.title}, ${callTwo?.price} ${callTwo?.thumbnail}}
-        callThree: [${callThree.map(obj => {return `{${obj['id']} ${obj['title']} ${obj['price']} ${obj['thumbnail']}}`} )}]`)
-    }
-    allCalls()
-  });
 
-  app.get('/productos', (req, res) => {
-    const file = new Container("Archivo");
-    const allCalls = async () => {
-      const products = await file.getAll()
-      res.send(`[${products.map(obj => {return `{${obj['id']} ${obj['title']} ${obj['price']} ${obj['thumbnail']}}`} )}]`)
-    }
-    allCalls()
-  });
-  
-  app.get('/productosRandom', (req, res) => {
-    const file = new Container("Archivo");
-    const allCalls = async () => {
-      const random = await file.getRandom()
-      res.send(`random: {${random?.id}, ${random?.title}, ${random?.price} ${random?.thumbnail}}`)
-    }
-    allCalls()
-  });
-//Comienza a escuchar el puerto definido 3000
-app.listen(time, ()=>{
-    console.log('Listen on the time 3000');
+products.get('/:id', (req, res)=> {
+  const id = req.params.id;
+  console.log("ID", id)
+  const file = new Container("Archivo");
+  const allCalls = async () => {
+    const callOne = await file.getById(id)
+    res.send(callOne)
+  }
+  allCalls()
+});
+products.get('/', (req, res)=> {
+  const file = new Container("Archivo");
+  const allCalls = async () => {
+    const callOne = await file.getAll()
+    res.send(callOne)
+  }
+  allCalls()
+});
+
+products.delete('/', (req, res)=> {
+  const id = req.params.id;
+  console.log("ID", id)
+  const file = new Container("Archivo");
+  const allCalls = async () => {
+    const callOne = await file.deleteById(id)
+    res.send(callOne)
+  }
+  allCalls()
+});
+
+app.listen(8080, () => {
+  console.log('Escuchando');
 });
